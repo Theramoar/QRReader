4eb727fa653dd2004c2c0d8ae76a3d2bd6ba860e
Mihails Kuznecovs <mihails.kuznecovs@testdevlab.com>
2021-01-28 11:39:24 +0200

    Added Network Layer

---
+++ QRReader/NetworkDataFetcher.swift
@@ -0,0 +1,22 @@
+//
+//  NetworkDataFetcher.swift
+//  QRReader
+//
+//  Created by MihailsKuznecovs on 28/01/2021.
+//
+
+import Foundation
+
+struct APIDummies {
+    let wrongAPIKey = "b7c85140"
+    let correctAPIKey = "b7c85141"
+}
+
+class NetworkDataFetcher {
+    private let network = NetworkService()
+    
+    func sendAPIKeyTo(url: String, key: String) {
+        
+    }
+    
+}
---
+++ QRReader/NetworkService.swift
@@ -0,0 +1,59 @@
+//
+//  NetworkService.swift
+//  QRReader
+//
+//  Created by MihailsKuznecovs on 28/01/2021.
+//
+
+import Foundation
+
+enum RequestType: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+class NetworkService {
+    private var session: URLSession = .shared
+    
+    func makeRequest(type: RequestType, path: String, queryItems: [URLQueryItem]? = nil, completion: @escaping (Data?) -> Void) {
+        guard let url = createUrl(from: path, queryItems: queryItems) else {
+            completion(nil)
+            return
+        }
+        let request = createRequest(type: type, url: url)
+        
+        let task = createDataTask(from: request) { data, error in
+            guard let data = data, error == nil else {
+                completion(nil)
+                return
+            }
+            completion(data)
+        }
+        task.resume()
+    }
+    
+    private func createUrl(from path: String, queryItems: [URLQueryItem]? = nil) -> URL? {
+        var components = URLComponents()
+        components.scheme = ""//API.scheme.rawValue
+        components.host = ""//API.host.rawValue
+        components.path = path
+        components.queryItems = queryItems
+        
+        return components.url
+    }
+    
+    private func createRequest(type: RequestType, url: URL) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.httpMethod = type.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        return request
+    }
+    
+    private func createDataTask(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) -> URLSessionDataTask {
+        session.dataTask(with: request) { data, _, error in
+            DispatchQueue.main.async {
+                completion(data, error)
+            }
+        }
+    }
+}
